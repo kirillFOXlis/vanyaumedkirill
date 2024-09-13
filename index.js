de63611dab09aec3bcc53d1d4b922d38c8d48b0c
Cybersport <Cybersport@26kadr.internal>
2024-09-13 15:45:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,89 @@
+
+//1
+//alert ("Я JavaScript")
+
+//2
+//let admin;
+//let name = 'Иван'
+//admin = name;
+//alert(admin); 
+
+
+//3 let name = "Ilya";
+//alert( hello ${1} ); // ?
+//alert( hello ${"name"} ); // ?
+//alert( hello ${name} ); // ?
+
+
+// var name = prompt("Как вас зовут?");
+//  document.write("Привет, " + Name + "!")';
+
+// let a = 1, b = 1;
+// let c = ++a; // ?
+// let d = b++; // ?
+
+
+// let a = 2;
+//let x = 1 + (a *= 2);
+//alert(a + b); // 12
+
+
+
+//7
+//let a = parseInt(prompt("Первое число?", 1));
+//let b = parseInt(prompt("Второе число?", 2));
+//alert(a + b); // 3
+
+
+//4
+//function greet() {
+//    const name =
+//document.getElementById('name').value;
+//    console.log(`Hello, ${name}!`);
+//}
+
+//8
+// 7 > 5
+// "стол" > стул
+// "4" >"52"
+//undefined ==null
+//undefined === null
+//null == "\n0\n"
+//null ==+ "\n0\n"
+
+//if ("0") {
+//   alert( "Привет");
+// }
+
+// let value = prompt("Какое "официальное" название JavaScript?", '');
+//     if (value == 'ECMAScript')
+//     {
+//        alert("Верно!");
+//     }
+//     else
+//     {
+//        alert("Не знаете? ECMAScript!");
+//     }
+
+// let value = prompt("Введите число")
+// if (value > 0)
+//  {
+//    alert(1);
+//  }
+//    else if (value < 0)
+//  {
+//    alert(-1);
+//  }
+//    else
+//  {
+//    alert(0);
+//  }
+
+let result;
+if (a +_b < 4) {
+    result = "Мало";
+}   else {
+    result = "Много";
+}
+
+
